778c0b5482fc8ce60ca7ca81133ac2f423026c32
keesha <kharg14@gmail.com>
2020-11-14 10:00:34 -0500

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,6 +1,6 @@
-class StudentsController < ApplicationController
-    def index
-        @students = Student.all
+class StudentsController < ApplicationController 
+    def students
+        @students = Student.all 
         render "index"
-    end
+    end 
 end
